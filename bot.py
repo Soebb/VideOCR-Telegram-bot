89220672829edf1ab81d4cdefd00165aebd1c282
Soebb <87156166+Soebb@users.noreply.github.com>
2026-05-18 23:06:24 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,58 @@
+from videocr import save_subtitles_to_file
+import os
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+load_dotenv()
+
+Bot = Client(
+    "videocrBot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+START_TXT = """
+Hi {}, I'm videocr Bot.
+
+Send a video with hard-coded subtitle to extract it in a srt file.
+"""
+
+START_BTN = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Source Code', url='https://github.com/soebb'),
+        ]]
+    )
+
+
+@Bot.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TXT.format(update.from_user.mention)
+    reply_markup = START_BTN
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+
+@Bot.on_message(filters.private & (filters.video | filters.document))
+async def from_tg_files(_, m):
+    if m.document and not m.document.mime_type.startswith("video/"):
+        return
+    msg = await m.reply("Downloading..")
+    media = await m.download()
+    await msg.edit_text("Processing..")
+    subs = subs_ai.transcribe(media, model)
+    output_name = "out.srt"
+    subs.save(output_name)
+    await m.reply_document(output_name)
+    await msg.delete()
+    os.remove(output_name)
+    os.remove(media)
+
+
+Bot.run()
